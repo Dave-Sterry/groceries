c4425efc44172689aa00496591406932eb9f2124
David Sterry <sterry.david@gmail.com>
2020-10-26 14:51:46 -0700

    add ForEAch and uppercase
    Co-authored-by: Jeff Chiu <jwchiu82@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,19 @@
 $(document).ready(function() {
-  $("#formOne").submit(function(){
-    const list =[];
-    
+  $("#formOne").submit(function(event){
+    const list=[];
+    list.push($("input#item1").val());
+    list.push($("input#item2").val());
+    list.push($("input#item3").val());
+    list.push($("input#item4").val());
+    list.push($("input#item5").val());
+    list.push($("input#item6").val());
+ 
+    list.sort();
+    let uppercaseList =[];
+    list.forEach(function(element) {
+      uppercaseList.push(element.toUpperCase());
+    });
+    console.log(uppercaseList);
+    event.preventDefault();
   });
 });
